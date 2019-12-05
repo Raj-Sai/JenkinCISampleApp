77fdc293b2fd652e6a879dbe9d6846c9aa879131
RajSai <amsarajm.acumen@gmail.com>
2019-12-05 15:31:43 +0700

    first commit

---
+++ JenkinCISampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JenkinCISampleApp
+//
+//  Created by Amsaraj Acumen on 12/5/19.
+//  Copyright © 2019 Amsaraj Acumen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
